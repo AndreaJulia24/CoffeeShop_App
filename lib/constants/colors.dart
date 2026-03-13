08891890d8137314c862017cdbe5e9b6aab437aa
AndreaJulia24 <czellecza@yahoo.com>
2026-03-13 02:17:16 +0200

    Ez meg csak az eleje, home_screen

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryBrown = Color(0xFF47362e);
+const Color primaryWhite = Color(0xFFece7e3);
+const Color secondaryBrown = Color(0xFF755a4a);
+const Color secondaryBrownDark = Color(0xFF3d2c24);
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:coffee_shop/constants/colors.dart';
+import 'package:coffee_shop/models/products.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isCoffeesSelected = true;
+  bool _isTeasSelected = false;
+  bool _isDrinksSelected = false;
+  bool _isCakesSelected = false;
+
+  void _selectCategory(String category) {
+    setState(() {
+      _isCoffeesSelected = category == 'Coffees';
+      _isTeasSelected = category == 'Teas';
+      _isDrinksSelected = category == 'Drinks';
+      _isCakesSelected = category == 'Cakes';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: primaryBrown,
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(Icons.menu, color: primaryWhite),
+                  const Text(
+                    "Hello, Andrea Czellecz",
+                    style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 20,
+                      color: primaryWhite,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const Icon(Icons.search, color: primaryWhite),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  buildCategoryItem(
+                    'Coffees',
+                    _isCoffeesSelected,
+                    () => _selectCategory('Coffees'),
+                  ),
+                  buildCategoryItem(
+                    'Teas',
+                    _isTeasSelected,
+                    () => _selectCategory('Teas'),
+                  ),
+                  buildCategoryItem(
+                    'Drinks',
+                    _isDrinksSelected,
+                    () => _selectCategory('Drinks'),
+                  ),
+                  buildCategoryItem(
+                    'Cakes',
+                    _isCakesSelected,
+                    () => _selectCategory('Cakes'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildCategoryItem(String name, bool isSelected, VoidCallback onTap) {
+  return GestureDetector(
+    onTap: onTap,
+    child: Column(
+      children: [
+        Text(
+          name,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            color: isSelected ? secondaryBrown : primaryWhite,
+          ),
+        ),
+        if (isSelected)
+          Container(
+            margin: const EdgeInsets.only(top: 5),
+            height: 2,
+            width: 55,
+            color: secondaryBrown,
+          ),
+      ],
+    ),
+  );
+}
+
+Widget buildCoffeeCards(Coffee coffee){
+  return Container(
+    )
+}
